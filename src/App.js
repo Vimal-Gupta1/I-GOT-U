0f291d8e259832e65b75c212651f0f182c583224
Vimal-Gupta1 <74982973+Vimal-Gupta1@users.noreply.github.com>
2022-04-28 14:24:15 +0530

    Basic Design of HOME

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import NavBar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import Home from "./components/pages/Home";
 function App() {
   return (
     <>
       <Router>
         <NavBar />
       </Router>
+      <Home />
     </>
   );
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,10 @@
 import React from "react";
+import "./Home.css";
+const logo = require("./Images/background-home.png");
 export default function Home() {
-  return <>Homes</>;
+  return (
+    <>
+      <img src={logo} height={976} width={1352} />
+    </>
+  );
 }
