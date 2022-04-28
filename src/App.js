519845a8d04ca90df9dd9eac4fcd2ac4de7cd62d
Vimal-Gupta1 <74982973+Vimal-Gupta1@users.noreply.github.com>
2022-04-28 13:47:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import NavBar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./components/pages/Home";
+import { About } from "./components/pages/About";
+import { Blog } from "./components/pages/Blog";
+import { Contact } from "./components/pages/Contact";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <NavBar />
+
+        <div className="pages">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/blog" component={Blog} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,11 @@
+import React from "react";
+export default function Contact() {
+  return (
+    <div className="Contact">
+      <img
+        src="https://pixabay.com/photos/mental-health-mental-wellness-mind-4232031/"
+        alt="Images"
+      />
+    </div>
+  );
+}
